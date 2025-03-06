907982a534fee4a1e64983b2ef4965428db645d0
Jakkawan <143472604+tey333@users.noreply.github.com>
2025-03-06 22:18:09 +0700

    เพิ่มตรวจสอบ User

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,15 +1,62 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { serialize } from "cookie";
+import { C_CTDoConnectToDatabase } from '../../database/connect_db';
+import { CEncrypt } from '../../../../hooks/CEncrypt';
 
 console.log("process login 2.1");
 const SECRET_KEY = process.env.SECRET_KEY as string;
 
+interface User {
+  FTUsrCode: string| null;
+  FTUsrLogin: string | null;
+  FTUsrLoginPwd: string | null;
+  FTUsrName: string | null;
+  FTBchCode: string | null;
+  FTAgnCode: string | null;
+  FTMerCode: string | null;
+}
+
 export async function POST(req: Request) {
   const { username, password } = await req.json();
   console.log("process login 2.2");
+  
   // 🛑 ตรวจสอบ Username/Password
-  if (username !== "admin" || password !== "password") {
+  const oPool = await C_CTDoConnectToDatabase();
+    const aResult = await oPool.request().query(`
+      SELECT DISTINCT
+        USRLI.FTUsrCode,
+        FTUsrLogin,
+		    FTUsrLoginPwd,
+	      USRL.FTUsrName,
+		    USRG.FTBchCode,
+        USRL.FTAgnCode,
+	      USRG.FTMerCode
+      FROM TCNMUsrLogin USRLI WITH (NOLOCK)
+      INNER JOIN TCNMUser_L USRL
+        ON USRL.FTUsrCode = USRLI.FTUsrCode
+	      AND USRL.FTAgnCode = USRLI.FTAgnCode
+      INNER JOIN TCNTUsrGroup USRG
+        ON USRG.FTUsrCode = USRLI.FTUsrCode
+	      AND USRL.FTAgnCode = USRLI.FTAgnCode
+      WHERE
+        FTUsrStaActive = '1'
+        AND FTUsrLogType = '1'
+        AND FTUsrLogin = '${username}'
+        AND USRL.FNLngID = 1
+        AND GETDATE() BETWEEN FDUsrPwdStart AND FDUsrPwdExpired
+      ORDER BY FTUsrCode ASC;
+     `);
+
+  const aData = aResult.recordset;
+  const oUserData = aData.map((user: User) => ({...user,}));
+  const tEncryptedPassword = new CEncrypt("2").C_PWDtASE128Encrypt(password);
+
+  const oUser = oUserData.find(
+    (oUserData) => oUserData.FTUsrLoginPwd === tEncryptedPassword
+  );
+
+  if (!oUser) {
     return new NextResponse(JSON.stringify({ message: "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" }), { status: 401 });
   }
 
