afca74b2bd209168d41777dfe1de7bc9ce1c1c35
Jakkawan <143472604+tey333@users.noreply.github.com>
2025-04-22 11:36:53 +0700

    เพิ่มเลขเวอร์ชั่น

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -393,7 +393,7 @@ export default function Login() {
         </form>
       </div>
 
-      <p className="text-center text-gray-400 text-sm mt-6">Version 1.0.11</p>
+      <p className="text-center text-gray-400 text-sm mt-6">Version 2.0.1</p>
       <p className="text-center text-gray-400 text-xs">© 2025 AdaPos+. All rights reserved.</p>
       <Image
         src="/icons/logoAdaLogin.png"
