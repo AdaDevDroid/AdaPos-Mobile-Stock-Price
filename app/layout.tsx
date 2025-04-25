f5399fe5ae8c67b70b227f562b4a54e4fdbe14b1
Jakkawan <143472604+tey333@users.noreply.github.com>
2025-04-25 10:11:21 +0700

    Revert "แก้ไข BASE_PATH เพิ่มเติม"
    This reverts commit 4677b11f2636fdcc1d71a2ab3368798bcb68c230.

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/manifest.json`} />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="icon" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`} />
       </head>
