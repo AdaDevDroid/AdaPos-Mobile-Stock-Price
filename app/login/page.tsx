0c7e1548c5bb489efc275d55cd4dcbe045b7ee29
Patwiat <patwiat@ada-soft.com>
2025-04-29 12:58:00 +0700

    แก้ไขค่า Qty ไม่ถูกต้อง

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -550,7 +550,7 @@ export default function Login() {
         </form>
       </div>
 
-      <p className="text-center text-gray-400 text-sm mt-6">Version 1.0.12_test</p>
+      <p className="text-center text-gray-400 text-sm mt-6">Version 1.0.12</p>
       <p className="text-center text-gray-400 text-xs">© 2025 AdaPos+. All rights reserved.</p>
 
 
