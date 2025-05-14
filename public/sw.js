dcf23e564f45367bf3303fc0593aa7d339e1d401
Jakkawan <143472604+tey333@users.noreply.github.com>
2025-05-14 17:03:51 +0700

    แก้ไข Static ไฟล์

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // นำเข้า Workbox
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-const VERSION = "1.0.2"; // เวอร์ชั่น
+const VERSION = "1.0.4"; // เวอร์ชั่น
 const BASE_PATH = self.__WB_MANIFEST?.BASE_PATH || '/AdaCheckStockSTD'; // กำหนด basePath ถ้ามี
 
 // ตรวจสอบว่า Workbox ถูกโหลดสำเร็จ
@@ -38,6 +38,7 @@ if (workbox) {
   async function updateStatic() {
     const cache = await caches.open('static-resources');
     const urls = [
+      `${BASE_PATH}/AdaFileServer/AdaPos5Dev/Adasoft/AdaFile/00039/company/250410133751648c47bae5d9128.png`,
       `${BASE_PATH}/_next/image?url=%2FAdaCheckStockSTD%2Ficons%2FlogoAdaLogin.png&w=256&q=75`,
       `${BASE_PATH}/_next/static/chunks/2170a4aa-49b5104fb06205c3.js`,
       `${BASE_PATH}/_next/static/chunks/463-26d0300b0cad1c8a.js`,
@@ -46,16 +47,23 @@ if (workbox) {
       `${BASE_PATH}/_next/static/chunks/626-e94f734356ed4534.js`,
       `${BASE_PATH}/_next/static/chunks/684-6a88eb906b4903b6.js`,
       `${BASE_PATH}/_next/static/chunks/7cb1fa1f-1822bc2611099e2d.js`,
+      `${BASE_PATH}/_next/static/chunks/899-33742bc382cdccc3.js`,
       `${BASE_PATH}/_next/static/chunks/836-53d164cd3468ff05.js`,
       `${BASE_PATH}/_next/static/chunks/8e1d74a4-fe16d9e17b9732e7.js`,
       `${BASE_PATH}/_next/static/chunks/aaea2bcf-ac6970987f5bc297.js`,
       `${BASE_PATH}/_next/static/chunks/app/layout-3e31d2da671e459a.js`,
+      `${BASE_PATH}/_next/static/chunks/app/login/page-ce2e8e7d282757d2.js`,
+      `${BASE_PATH}/_next/static/chunks/app/main/page-52305ece46667372.js	`,
       `${BASE_PATH}/_next/static/chunks/app/login/page-3f97dd2ce14bb1b0.js`,
       `${BASE_PATH}/_next/static/chunks/app/main/page-1af1dffdd2bbfcc9.js`,
       `${BASE_PATH}/_next/static/chunks/app/page-e5d451557ffaafa7.js`,
+      `${BASE_PATH}/_next/static/chunks/app/price-check/page-fbdfe8ede86c43ce.js`,
       `${BASE_PATH}/_next/static/chunks/app/price-check/page-7c28b0048cba184b.js`,
+      `${BASE_PATH}/_next/static/chunks/app/receive/page-790ab52e8241ac1f.js`,
       `${BASE_PATH}/_next/static/chunks/app/receive/page-ee74d62e9c3d1d24.js`,
+      `${BASE_PATH}/_next/static/chunks/app/stock/page-2f193d9b59c9e421.js`,
       `${BASE_PATH}/_next/static/chunks/app/stock/page-3cdd7eea61b8daa1.js`,
+      `${BASE_PATH}/_next/static/chunks/app/transfer/page-ba9452abe9f64878.js`,
       `${BASE_PATH}/_next/static/chunks/app/transfer/page-a25d77e1689ba8c4.js`,
       `${BASE_PATH}/_next/static/chunks/e34aaff9-9d0e4ab542d5999d.js`,
       `${BASE_PATH}/_next/static/chunks/ee560e2c-8a73055faacac193.js`,
